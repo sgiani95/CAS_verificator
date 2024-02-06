aadc696a46fcc6f737cdcd404e3f058fc097a867
sgiani95 <157724093+sgiani95@users.noreply.github.com>
2024-02-06 17:36:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 # Configure logging to output to a file
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
+# Now, you can use logging.info(), logging.warning(), etc. to log messages to 'app.log'
+logging.info('This is an informational message')
+logging.warning('This is a warning message')
+
 # Route for processing CAS numbers
 @app.route('/process', methods=['POST'])
 def process_cas_number():
