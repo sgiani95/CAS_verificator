eb0dad263d403c9967b93b1776e306f63e645448
sgiani95 <157724093+sgiani95@users.noreply.github.com>
2024-02-07 10:03:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
- function queryNumber() {
-     var number = document.getElementById("numberInput").value;
-     // Send the number to the backend server for querying
-     var xhr = new XMLHttpRequest();
-     xhr.open("POST", "/query", true);
-     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-     xhr.onreadystatechange = function () {
-         if (xhr.readyState === XMLHttpRequest.DONE) {
-             if (xhr.status === 200) {
-                 document.getElementById("result").innerText = xhr.responseText;
-             } else {
-                 console.error('Error:', xhr.status);
-             }
-         }
-     };
-     xhr.send("number=" + encodeURIComponent(number));
- }
+function queryNumber() {
+    var number = document.getElementById("numberInput").value;
+    // Send the number to the backend server for querying
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/query", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var result = xhr.responseText;
+                document.getElementById("result").innerText = result;
+            } else {
+                console.error('Error:', xhr.status);
+            }
+        }
+    };
+    xhr.send("number=" + encodeURIComponent(number));
+}
