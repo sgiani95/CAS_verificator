168353405cea26a4b79193876672d7ac55547b54
sgiani95 <157724093+sgiani95@users.noreply.github.com>
2024-02-07 09:47:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Your other routes and functions go here...
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/query', methods=['POST'])
+def query_number():
+    number = request.form['number']
+    # Placeholder response for now
+    return 'Querying number: {}'.format(number)
 
 if __name__ == '__main__':
     app.run(debug=True)
